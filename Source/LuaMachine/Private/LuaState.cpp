302d9874a2fc6f853b4992cad27b7d4641e27e57
benmucha <43706062+benmucha@users.noreply.github.com>
2026-02-03 01:24:36 -0500

    ignore MetaTableFunctionUserData__gc

--- Source/LuaMachine/Private/LuaState.cpp
+++ Source/LuaMachine/Private/LuaState.cpp
@@ -1350,7 +1350,7 @@ int ULuaState::MetaTableFunctionUserData__gc(lua_State* L)
 	{
 		// My edit: If the UObject was already GC'd by UE, that's fine — nothing to do.
 		// - Hm idk tho, i do NewLuaUserDataObject(ULuaEntity::StaticClass(), true);, so it's tracked in uproperty tarray. -- Ah well hopefully it's j not entity objs doing this, rn i have ULuaEntPathOption which isn't tracked, so hopefully from that.
-		UE_LOG(LogTemp, Log, TEXT("ULuaState MetaTableFunctionUserData__gc: invalid UObject for UserData"));
+		// UE_LOG(LogTemp, Log, TEXT("ULuaState MetaTableFunctionUserData__gc: invalid UObject for UserData"));
 		return 0;
 		// LUAMACHINE_RETURN_ERROR(L, "invalid UObject for UserData %p", UserData);
 	}
